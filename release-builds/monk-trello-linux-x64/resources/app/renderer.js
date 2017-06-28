9f014ec07e12616c224775e7df033d189f7910ca
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-06-28 16:17:36 -0300

    app linux

---
+++ release-builds/monk-trello-linux-x64/resources/app/renderer.js
@@ -0,0 +1,18 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+onload = () => {
+    const webview = document.querySelector('webview')
+    const indicator = document.querySelector('.indicator')
+
+    const loadstart = () => {
+      indicator.innerText = 'loading...'
+    }
+
+    const loadstop = () => {
+      indicator.innerText = ''
+    }
+
+    webview.addEventListener('did-start-loading', loadstart)
+    webview.addEventListener('did-stop-loading', loadstop)
+  }
